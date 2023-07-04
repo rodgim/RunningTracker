b966b661d0e309d67bbdf5f415822faac405e64d
Rodrigo Gimenez <rodrigo_g90@hotmail.es>
2023-07-04 13:27:11 -0400

    setup Timber and remove unused library

--- app/src/main/java/com/rodgim/runningtracker/RunningTrackerApplication.kt
+++ app/src/main/java/com/rodgim/runningtracker/RunningTrackerApplication.kt
@@ -2,6 +2,13 @@ package com.rodgim.runningtracker
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class RunningTrackerApplication : Application()
+class RunningTrackerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
